8a4eb921a299e9364b3ec79d8fd0785727560346
RJ Xu <Rongji.Xu@outlook.com>
2022-02-15 14:07:53 +0800

    Update reg32.h

--- reg32.h
+++ reg32.h
@@ -9,7 +9,7 @@
 // Mask (from x for n bits)
 #define MASK32(x, n) (((uint32_t)(n) >= sizeof(uint32_t) * CHAR_BIT) ? (uint32_t)-1 : ((1UL << (n)) - 1) << (x))
 // Mask (closed interval [x, y])
-#define MASK32_CLOSED(x, y) MASK32(x, y - x + 1)
+#define MASK32_CLOSED(x, y) MASK32((x), (y) - (x) + 1)
 
 #define BIT32(bit) (1UL << (bit))
 // Find the first set bit (31-0)
@@ -20,7 +20,6 @@
 #define BIT32_COUNT(mask)       (__builtin__popcount(mask))
 #define BIT32_MASK_OF_FFS(mask) ((mask) & -(mask))
 #define BIT32_IS_CONTINUOUS(mask) (BIT32_FLS(mask) - BIT32_FFS(mask) + 1 == BIT32_COUNT(mask))
-#define BITS32(start,end) ((uint32_t)-1)
 
 #define REG32(reg)           (*(volatile uint32_t *)(reg))
 #define REG32_GET(reg, mask) (REG32(reg) & (mask))
@@ -36,7 +35,7 @@
   do {                          \
     REG32(reg) ^= (mask);       \
   } while (0)
-#define REG32_BOOL(reg, mask) ((bool)(!!REG32_GET(reg, mask)))
+#define REG32_BOOL(reg, mask) ((bool)(!!REG32_GET((reg), (mask))))
 
 #define REG32_SWITCH(reg, enable) \
   do {                            \
@@ -57,16 +56,22 @@
     REG32(reg) = (uint32_t)(val); \
   } while (0)
 
-#define REG32_FIELD_GET_BITS(reg, x, n) ((int)REG32_GET(reg, MASK32(x, n)) >> (x))
+#define REG32_FIELD_GET_BITS(reg, x, n) ((int)REG32_GET((reg), MASK32((x), (n))) >> (x))
 #define REG32_FIELD_SET_BITS(reg, x, n, val)                                             \
   do {                                                                                   \
-    REG32(reg) = (uint32_t)(REG32(reg) & (~MASK32(x, n)) | ((val & MASK(0, n)) << (x))); \
+    REG32(reg) = (uint32_t)(REG32(reg) & (~MASK32((x), (n))) | (((val) & MASK(0, (n))) << (x))); \
   } while (0)
-#define REG32_FIELD_GET(reg, mask)      ((int)REG32_GET(reg, mask) >> BIT32_FFS(mask))
-#define REG32_FIELD_SET(reg, mask, val) REG32_FIELD_SET_BITS(reg, BIT32_FFS(mask), BIT32_COUNT(mask), val)
+#define REG32_FIELD_GET(reg, mask)      ((int)REG32_GET((reg), (mask)) >> BIT32_FFS(mask))
+#define REG32_FIELD_SET(reg, mask, val) REG32_FIELD_SET_BITS((reg), BIT32_FFS(mask), BIT32_COUNT(mask), (val))
 
-#define REG32_GET_BIT(reg, bit)    REG32_GET(reg, BIT32(bit))
-#define REF32_SET_BIT(reg, bit)    REG32_SET(reg, BIT32(bit))
-#define REG32_CLEAR_BIT(reg, bit)  REG32_CLEAR(reg, BIT32(bit))
-#define REG32_TOGGLE_BIT(reg, bit) REG32_TOGGLE(reg, BIT32(bit))
-#define REG32_BOOL_BIT(reg, bit)   REG32_BOOL(reg, BIT32(bit))
+#define REG32_BIT_GET(reg, bit)    REG32_GET((reg), BIT32(bit))
+#define REF32_BIT_SET(reg, bit)    REG32_SET((reg), BIT32(bit))
+#define REG32_BIT_CLEAR(reg, bit)  REG32_CLEAR((reg), BIT32(bit))
+#define REG32_BIT_TOGGLE(reg, bit) REG32_TOGGLE((reg), BIT32(bit))
+#define REG32_BIT_BOOL(reg, bit)   REG32_BOOL((reg), BIT32(bit))
+
+#define REG32_BYTE_GET(reg, byte)    REG32_GET((reg), MASK32((byte) * CHAR_BIT, CHAR_BIT))
+#define REG32_BYTE_SET(reg, byte)    REG32_SET((reg), MASK32((byte) * CHAR_BIT, CHAR_BIT))
+#define REG32_BYTE_CLEAR(reg, byte)  REG32_CLEAR((reg), MASK32((byte) * CHAR_BIT, CHAR_BIT))
+#define REG32_BYTE_TOGGLE(reg, byte) REG32_TOGGLE((reg), MASK32((byte) * CHAR_BIT, CHAR_BIT))
+#define REG32_BYTE_BOOL(reg, byte)   REG32_BOOL((reg), MASK32((byte) * CHAR_BIT, CHAR_BIT))
