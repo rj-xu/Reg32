0a4b890b28849bd7bbca8de8c3f6b9e6d0f8846c
RJ Xu <Rongji.Xu@outlook.com>
2024-01-04 23:30:27 +0800

    🐛 FIX:

--- reg32.h
+++ reg32.h
@@ -28,106 +28,108 @@
 // Bit
 /////////////////////////////////////////////////
 
-#define BIT32(bit) (1UL << (bit))
+#define BIT(bit) (1UL << (bit))
 // Find the first set bit (31-0)
-#define BIT32_FFS(mask) (__builtin_ffs(mask) - 1)
+#define BIT_FFS(mask) (__builtin_ffs(mask) - 1)
 // Find the last set bit (31-0)
-#define BIT32_FLS(mask) (31 - __builtin_clz(mask))
+#define BIT_FLS(mask) (31 - __builtin_clz(mask))
 // Count the set bits
-#define BIT32_CNT(mask) __builtin__popcount(mask)
+#define BIT_CNT(mask) __builtin__popcount(mask)
 
-#define BIT32_MASK_OF_FIRST_SET(mask) ((mask) & -(mask))
+#define BIT_MASK_OF_FIRST_SET(mask) ((mask) & -(mask))
 
-#define BIT32_IS_CONTINUOUS(mask) \
-  ((BIT32_FLS(mask) - BIT32_FFS(mask) + 1) == BIT32_CNT(mask))
-
-#define BYTE4(byte) MASK32((byte) * 8, 8)
+#define BIT_IS_CONTINUOUS(mask) \
+  ((BIT_FLS(mask) - BIT_FFS(mask) + 1) == BIT_CNT(mask))
 
 /////////////////////////////////////////////////
 // Mask
 /////////////////////////////////////////////////
 
 // Mask (from x for n bits)
-#define MASK32(x, n) (((1UL << (n)) - 1) << (x))
+#define MASK(x, n) (((1UL << (n)) - 1) << (x))
 // Mask (closed interval [x, y])
-#define MASK32_SQ(x, y) MASK32((x), (y) - (x) + 1)
+#define MASK_SQ(x, y) MASK((x), (y) - (x) + 1)
 
-#define MASK32_VA(x, args...)                    \
-  ({                                             \
-    uint32_t _m = 0;                             \
-    for (int i = 0; i < COUNT_ARGS(##args); i++) \
-    {                                            \
-      _m |= BIT32(i);                            \
-    }                                            \
-    _m;                                          \
-  })
+// // TODO
+// #define MASK_VA(args...)                       \
+//   ({                                             \
+//     uint32_t _mask = 0;                          \
+//     for (int i = 0; i < COUNT_ARGS(##args); i++) \
+//     {                                            \
+//       _mask |= BIT(i);                         \
+//     }                                            \
+//     _m;                                          \
+//   })
+
+#define MASK_BYTE(byte) MASK((byte) * 8, 8)
+#define BYTE32(byte, mask) ((mask) << ((byte) * 8))
 
 /////////////////////////////////////////////////
 // Reg
 /////////////////////////////////////////////////
 
-#define REG32(reg) (*(volatile uint32_t *)(reg))
+#define REG(reg) (*(volatile uint32_t *)(reg))
 
-#define REG32_READ(reg) REG32(reg)
-#define REG32_WRITE(reg, val)     \
-  do                              \
-  {                               \
-    REG32(reg) = (uint32_t)(val); \
-  } while (0)
-
-#define REG32_ENABLE(reg) \
-  do                      \
-  {                       \
-    REG32(reg) = 1;       \
-  } while (0)
-#define REG32_DISABLE(reg) \
-  do                       \
-  {                        \
-    REG32(reg) = 0;        \
-  } while (0)
-
-#define REG32_GET(reg, mask) (REG32(reg) & (mask))
-#define REG32_SET(reg, mask) \
-  do                         \
-  {                          \
-    REG32(reg) |= (mask);    \
-  } while (0)
-#define REG32_CLEAR(reg, mask) \
-  do                           \
-  {                            \
-    REG32(reg) &= ~(mask);     \
-  } while (0)
-
-#define REG32_TOGGLE(reg, mask) \
+#define REG_READ(reg) REG(reg)
+#define REG_WRITE(reg, val)     \
   do                            \
   {                             \
-    REG32(reg) ^= (mask);       \
+    REG(reg) = (uint32_t)(val); \
   } while (0)
 
-#define REG32_TRIGGER(reg, mask)   \
-  do                               \
-  {                                \
-    uint32_t _v = REG32_READ(reg); \
-    REG32(reg) = _v | (mask);      \
-    REG32(reg) = _v & (~(mask));   \
+#define REG_ENABLE(reg) \
+  do                    \
+  {                     \
+    REG(reg) = 1;       \
+  } while (0)
+#define REG_DISABLE(reg) \
+  do                     \
+  {                      \
+    REG(reg) = 0;        \
   } while (0)
 
-#define REG32_GET_BITS(reg, x, n) \
-  (REG32_GET((reg), MASK32((x), (n))) >> (x))
-#define REG32_SET_BITS(reg, x, n, val)                \
-  do                                                  \
-  {                                                   \
-    REG32(reg) =                                      \
-        (uint32_t)(REG32(reg) & (~MASK32((x), (n))) | \
-                   (((val) & MASK(0, (n))) << (x)));  \
+#define REG_GET(reg, mask) (REG(reg) & (mask))
+#define REG_SET(reg, mask) \
+  do                       \
+  {                        \
+    REG(reg) |= (mask);    \
+  } while (0)
+#define REG_CLEAR(reg, mask) \
+  do                         \
+  {                          \
+    REG(reg) &= ~(mask);     \
+  } while (0)
+
+#define REG_TOGGLE(reg, mask) \
+  do                          \
+  {                           \
+    REG(reg) ^= (mask);       \
+  } while (0)
+
+#define REG_TRIGGER(reg, mask)   \
+  do                             \
+  {                              \
+    uint32_t _v = REG_READ(reg); \
+    REG(reg) = _v | (mask);      \
+    REG(reg) = _v & (~(mask));   \
+  } while (0)
+
+#define REG_GET_BITS(reg, x, n) \
+  (REG_GET((reg), MASK((x), (n))) >> (x))
+#define REG_SET_BITS(reg, x, n, val)                 \
+  do                                                 \
+  {                                                  \
+    REG(reg) =                                       \
+        (uint32_t)(REG(reg) & (~MASK((x), (n))) |    \
+                   (((val) & MASK(0, (n))) << (x))); \
   } while (0)
 
 /////////////////////////////////////////////////
 // Big/Little Endian
 /////////////////////////////////////////////////
 
-#define BE_TO_LE32(x)                                   \
+#define BE_TO_LE(x)                                   \
   ((0x000000FF & (x) << 24) | (0x0000FF00 & (x) << 8) | \
    (0x00FF0000 & (x) >> 8) | (0xFF000000 & (x) >> 24))
 
-#define LE_TO_BE32(x) BE_TO_LE32(x)
+#define LE_TO_BE(x) BE_TO_LE(x)
